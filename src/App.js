efe39b7e941a0b840f8037bb99cefe9588b0fa50
Alexey Gladyshev <alexey0gladyshev@gmail.com>
2017-11-02 16:13:47 +0300

    refactor: Divide React components into files according to routes

--- src/App.js
+++ src/App.js
@@ -1,57 +1,8 @@
-import React from 'react'
+import React from 'react';
 // import * as BooksAPI from './BooksAPI'
+import Library from './Library'
+import Search from './Search'
 import './App.css'
-
-class Search extends React.Component {
-  render() {
-    return (
-      <div className="search-books">
-        <div className="search-books-bar">
-          <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-          <div className="search-books-input-wrapper">
-            {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
-            <input type="text" placeholder="Search by title or author"/>
-          </div>
-        </div>
-        <div className="search-books-results">
-          <ol className="books-grid"></ol>
-        </div>
-      </div>
-    ) 
-  }
-}
-
-const Book = (props) => (
-  <li>
-    <div className="book">
-      <div className="book-top">
-        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url(' + props.book.url + ')' }}></div>
-        <ShelfChanger shelf={props.book.shelf}/>
-      </div>
-      <div className="book-title">{props.book.title}</div>
-      <div className="book-authors">{props.book.author}</div>
-    </div>
-  </li>  
-);
-
-const ShelfChanger = (props) => (
-  <div className="book-shelf-changer">
-    <select value={props.shelf}>
-      <option value="none" disabled>Move to...</option>
-      <option value="currentlyReading">Currently Reading</option>
-      <option value="wantToRead">Want to Read</option>
-      <option value="read">Read</option>
-      <option value="none">None</option>
-    </select>
-  </div>
-);
 
 const BOOKS = [
   {
@@ -80,73 +31,6 @@ const BOOKS = [
   }
 ]
 
-class Bookshelf extends React.Component {
-  render() {
-    console.log(this.props);
-    const shelf = [];
-    this.props.books.forEach((book) => {
-      shelf.push(
-        <Book
-          book={book}
-          key={book.title}
-        />
-      );
-    });
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{this.props.shelfName}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {shelf}
-          </ol>
-        </div>
-      </div>
-    )
-  }
-}
-
-class Library extends React.Component {
-  render() {
-    const shelves = new Map([]);
-    this.props.books.forEach((book) => {
-      if (shelves.has(book.shelf)) {
-        const shelf = shelves.get(book.shelf);
-        shelf.push(book);
-        shelves.set(book.shelf, shelf);
-      } else {
-        shelves.set(book.shelf, [book]);
-      }}
-    )
-
-    const bookshelves = [];
-
-    shelves.forEach(function(shelf, key) {
-      bookshelves.push(
-        <Bookshelf
-          books={shelf}
-          shelfName={key}
-          key={key} />
-      );
-    });
-
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <div>
-            {bookshelves}
-          </div>
-        </div>
-        <div className="open-search">
-          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-        </div>
-      </div>
-    )
-  }
-}
-
 class BooksApp extends React.Component {
   state = {
     showSearchPage: false
@@ -160,7 +44,5 @@ class BooksApp extends React.Component {
     )
   }
 }
-
-
 
 export default BooksApp
---
+++ src/Library.js
@@ -0,0 +1,94 @@
+import React from 'react';
+
+const Book = (props) => (
+  <li>
+    <div className="book">
+      <div className="book-top">
+        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url(' + props.book.url + ')' }}></div>
+        <ShelfChanger shelf={props.book.shelf}/>
+      </div>
+      <div className="book-title">{props.book.title}</div>
+      <div className="book-authors">{props.book.author}</div>
+    </div>
+  </li>  
+);
+
+const ShelfChanger = (props) => (
+  <div className="book-shelf-changer">
+    <select value={props.shelf}>
+      <option value="none" disabled>Move to...</option>
+      <option value="currentlyReading">Currently Reading</option>
+      <option value="wantToRead">Want to Read</option>
+      <option value="read">Read</option>
+      <option value="none">None</option>
+    </select>
+  </div>
+);
+
+class Bookshelf extends React.Component {
+  render() {
+    const shelf = [];
+    this.props.books.forEach((book) => {
+      shelf.push(
+        <Book
+          book={book}
+          key={book.title}
+        />
+      );
+    });
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{this.props.shelfName}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {shelf}
+          </ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+class Library extends React.Component {
+  render() {
+    const shelves = new Map([]);
+    this.props.books.forEach((book) => {
+      if (shelves.has(book.shelf)) {
+        const shelf = shelves.get(book.shelf);
+        shelf.push(book);
+        shelves.set(book.shelf, shelf);
+      } else {
+        shelves.set(book.shelf, [book]);
+      }}
+    )
+
+    const bookshelves = [];
+
+    shelves.forEach(function(shelf, key) {
+      bookshelves.push(
+        <Bookshelf
+          books={shelf}
+          shelfName={key}
+          key={key} />
+      );
+    });
+
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            {bookshelves}
+          </div>
+        </div>
+        <div className="open-search">
+          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Library
