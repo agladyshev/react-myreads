5889f879e8ccf24957c2a158c551e5923ad84474
Alexey Gladyshev <alexey0gladyshev@gmail.com>
2017-11-03 17:03:48 +0300

    feat: disable None shelf choice if book isn't in the library

--- src/ShelfChanger.js
+++ src/ShelfChanger.js
@@ -19,14 +19,17 @@ class ShelfChanger extends React.Component {
   }
 
   render() {
+    const shelf = this.props.shelf;
     return (
       <div className="book-shelf-changer">
-        <select value={this.props.shelf} onChange={this.handleChange}>
+        <select value={shelf} onChange={this.handleChange}>
           <option value="none" disabled>Move to...</option>
           <option value="currentlyReading">Currently Reading</option>
           <option value="wantToRead">Want to Read</option>
           <option value="read">Read</option>
-          <option value="none">None</option>
+          {(shelf) &&
+            <option value="none">None</option>
+          }
         </select>
       </div>
     );
